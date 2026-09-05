232433cd37b99f3ae7988dd0bd39181e218c473a
Srinjoy <srinjoypramanik01me@gmail.com>
2026-09-06 00:32:32 +0600

    override widget build

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -3,4 +3,8 @@ import 'package:flutter/material.dart';
 class Homescreen extends StatelessWidget{
   const Homescreen({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
 }
