bd3c9f910681405d9df8cc811c789f54e8255e4b
Srinjoy <srinjoypramanik01me@gmail.com>
2026-09-08 03:51:27 +0600

    Update splashscreen.dart

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'homescreen.dart';
 
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Go to Home'
+        ),
+      ),
+    );
+  }
 }
 
