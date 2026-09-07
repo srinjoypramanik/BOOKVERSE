22d067062af67d424cfc56861988d567d9c0e0d5
Sheikh Tanzid Ahmed Sadi <tanzid01716@gmail.com>
2026-09-07 12:39:10 +0600

    Update sign_up.dart

--- lib/sign_up.dart
+++ lib/sign_up.dart
@@ -2,4 +2,14 @@ import 'package:flutter/material.dart';
 
 class SignUpPage extends StatelessWidget{
  const SignUpPage({super.key});
+ 
+ @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      body: Center(
+        child: Text('SignUp page'),
+      ),
+    );
+  }
 }
