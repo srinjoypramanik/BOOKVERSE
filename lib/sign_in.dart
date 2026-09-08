4935584f25a670343b0e6677259ac951bee60c95
Sheikh Tanzid Ahmed Sadi <tanzid01716@gmail.com>
2026-09-08 20:21:29 +0600

    Update sign_in.dart

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SignIn extends StatelessWidget{
-  const SignIn({super.key})
+  const SignIn({super.key});
 
   @override
   Widget build(BuildContext context) {
