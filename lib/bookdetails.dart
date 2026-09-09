04f37a10575141522ad4108082b6a48410695c9b
Srinjoy <srinjoypramanik01me@gmail.com>
2026-09-09 22:24:40 +0600

    Update bookdetails.dart

--- lib/bookdetails.dart
+++ lib/bookdetails.dart
@@ -2,4 +2,11 @@ import 'package:flutter/material.dart';
 
 class BookDetails extends StatelessWidget{
   const BookDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
 }
