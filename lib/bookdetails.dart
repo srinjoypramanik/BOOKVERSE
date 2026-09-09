9203e389815aef9789b2e93663fdbfb85ff52553
Srinjoy <srinjoypramanik01me@gmail.com>
2026-09-09 22:29:33 +0600

    Update bookdetails.dart

--- lib/bookdetails.dart
+++ lib/bookdetails.dart
@@ -6,6 +6,15 @@ class BookDetails extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
+
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+      ),
+
+      body: Center(),
+
+      
 
     );
   }
