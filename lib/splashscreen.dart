c6a02b93283c81b384efd6ba92eb15e851c8f1f5
Srinjoy <srinjoypramanik01me@gmail.com>
2026-09-09 22:06:27 +0600

    Update splashscreen.dart

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'homescreen.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
+
+  @override
+  state<SplashScreen> createState() => SplashScreenState();
+}
+
+class SplashScreenState extends state<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
