3ff8d7f193dde0ff2b9bb3d36a52be7916bf22a8
Srinjoy <srinjoypramanik01me@gmail.com>
2026-09-09 21:21:47 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+import 'splashscreen.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+}
