37684ac0a9ae4d806a761497481d7f84015b83a3
Srinjoy <srinjoypramanik01me@gmail.com>
2026-09-12 00:52:40 +0600

    book details app bar and navigation

--- lib/bookdetails.dart
+++ lib/bookdetails.dart
@@ -1,3 +1,4 @@
+import 'package:bookverse/sign_in.dart';
 import 'package:flutter/material.dart';
 
 class BookDetails extends StatelessWidget{
@@ -6,19 +7,96 @@ class BookDetails extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.white,
 
       appBar: AppBar(
         backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.arrow_back,
+              color: Colors.black,
+              size: 35),
+        ),
+
+        centerTitle: true,
+        title: Text('BOOK METRICS',
+          style: TextStyle(
+            color: Colors.grey,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+           ),
+          ),
+
+
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.shopping_bag_outlined,
+              color: Colors.black,
+              size: 35,
+            ),
+          ),
+
+          const SizedBox(width: 6),
+        ],
       ),
 
-      body: Center(),
+      
+
+
+
+
+
 
       bottomNavigationBar: BottomNavigationBar(
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: Colors.white,
-          items: const[]),
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.white,
 
+        // Tanzid........................................
+        onTap: (index){
+          if(index == 3){
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context)=> const SignIn()
+                )
+            );
+          }
+        },
+        //Tanzid.........................................
+
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_filled,
+              size: 30,
+              color: Colors.black,
+            ),
+            label: 'HOME',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search_outlined,
+              size: 30,
+              color:Colors.black,
+            ),
+            label: 'STORE',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag_rounded,
+              size: 30,
+              color:Colors.black,
+            ),
+            label: 'CART',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_2_rounded,
+              size: 30,
+              color:Colors.black,
+            ),
+            label: 'AUTH',
+          ),
+        ],
+      ),
     );
   }
 }
