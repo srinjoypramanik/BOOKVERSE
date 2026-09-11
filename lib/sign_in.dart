e545acdf924f34ebeb37c1ae395bf7d312fc91d5
Sheikh Tanzid Ahmed Sadi <tanzid01716@gmail.com>
2026-09-12 00:25:44 +0600

    Update sign_in.dart

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -61,7 +61,7 @@ class SignIn extends StatelessWidget{
               fillColor: Color(0xFFF1F1F5),
 
               border: OutlineInputBorder(
-                
+                borderRadius: BorderRadius.circular(20),
               ),
             ),
           ),
