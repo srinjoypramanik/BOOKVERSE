7ec8451f2ca38bbce2a19efce17aa9a4393b0160
Srinjoy <srinjoypramanik01me@gmail.com>
2026-09-12 00:55:12 +0600

    Update bookdetails.dart

--- lib/bookdetails.dart
+++ lib/bookdetails.dart
@@ -127,7 +127,33 @@ class BookDetails extends StatelessWidget{
                  ],
                ),
 
-               
+               Row(
+                 children: [
+                   Column(
+                     crossAxisAlignment: CrossAxisAlignment.end,
+                     children: [
+                       Text('CATAGORY',
+                         style: TextStyle(
+                           color: Colors.grey,
+                           fontWeight: FontWeight.bold,
+                           fontSize: 14,
+                         ),
+                       ),
+                       SizedBox(height: 8),
+                       Text('COMPUTER SCIENCE',
+                         style: TextStyle(
+                           color: Colors.black,
+                           fontSize: 18,
+                         ),
+                       ),
+                     ],
+                   ),
+                   SizedBox(width: 12),
+                 ],
+               ),
+             ],
+           ),
+           
 
 
          ],
