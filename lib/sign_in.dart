28b56a868665188f92033729cf90f4c8c967d1a9
Sheikh Tanzid Ahmed Sadi <tanzid01716@gmail.com>
2026-09-12 01:05:08 +0600

    Update sign_in.dart

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -161,6 +161,7 @@ class SignIn extends StatelessWidget{
                 child: Text('Create a new account',
                   style: TextStyle(
                     fontSize: 20,
+                    
                   ),
                 ),
               ),
