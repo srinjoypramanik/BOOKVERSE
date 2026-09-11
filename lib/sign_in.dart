5e4361dbb92b15d620d73c6a70589c5db6ff624b
Sheikh Tanzid Ahmed Sadi <tanzid01716@gmail.com>
2026-09-12 01:02:39 +0600

    Update sign_in.dart

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -153,7 +153,9 @@ class SignIn extends StatelessWidget{
                   );
                 },
 
-                
+                style: TextButton.styleFrom(
+                  
+                ),
               ),
             ),
           ),
