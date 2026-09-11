e66cf38ba3114a33e62fd180b980350a1ac73c29
Sheikh Tanzid Ahmed Sadi <tanzid01716@gmail.com>
2026-09-12 00:33:34 +0600

    Update sign_in.dart

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -80,7 +80,7 @@ class SignIn extends StatelessWidget{
           SizedBox(height: 10),
           TextField(
             decoration: InputDecoration(
-              
+              hintText: 'Enter your Password',
             ),
           ),
         ],
