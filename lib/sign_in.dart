1a69ab6c337fabf049881215c5e4cd5be55f4448
Sheikh Tanzid Ahmed Sadi <tanzid01716@gmail.com>
2026-09-14 23:48:19 +0600

    Fix

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'sign_up.dart';
 
 class SignIn extends StatelessWidget {
-
   const SignIn({super.key});
 
 
@@ -42,38 +41,74 @@ class SignIn extends StatelessWidget {
 
       body: SafeArea(
 
-        child: SingleChildScrollView(
+        child: Padding(
 
-          child: Padding(
+          padding: EdgeInsets.symmetric(
 
-            padding: EdgeInsets.symmetric(
+            horizontal: 20,
 
-              horizontal: 20,
-
-            ),
+          ),
 
 
-            child: Column(
+          child: Column(
 
-              crossAxisAlignment: CrossAxisAlignment.start,
-
-
-              children: [
+            crossAxisAlignment: CrossAxisAlignment.start,
 
 
-                SizedBox(
+            children: [
 
-                  height: 100,
 
-                  child: Center(
 
-                    child: Image.asset(
+              SizedBox(
 
-                      'assets/images/bookverse.png',
+                height: 100,
 
-                      height: 300,
+                child: Center(
 
-                      width: 300,
+                  child: Image.asset(
+
+                    'assets/images/bookverse.png',
+
+                    height: 300,
+
+                    width: 300,
+
+                  ),
+
+                ),
+
+              ),
+
+
+
+
+
+              SizedBox(height: 20),
+
+
+
+
+
+              Center(
+
+                child: TextButton(
+
+                  onPressed: (){
+
+                  },
+
+
+                  child: Text(
+
+                    'Sign in with your email',
+
+                    style: TextStyle(
+
+                      fontSize: 20,
+
+                      fontWeight: FontWeight.w900,
+
+                      decoration: TextDecoration.underline,
 
                     ),
 
@@ -81,193 +116,159 @@ class SignIn extends StatelessWidget {
 
                 ),
 
-
-
-
-                SizedBox(height: 20),
+              ),
 
 
 
 
 
-                Center(
-
-                  child: TextButton(
-
-                    onPressed: (){
-
-                    },
+              SizedBox(height: 20),
 
 
-                    child: Text(
-
-                      'Sign in with your email',
 
 
-                      style: TextStyle(
 
-                        fontSize: 20,
+              Text(
 
-                        fontWeight: FontWeight.w900,
+                'Email Address:',
 
-                        decoration: TextDecoration.underline,
 
-                      ),
+                style: TextStyle(
 
-                    ),
+                  fontSize: 18,
+
+                  fontWeight: FontWeight.bold,
+
+                ),
+
+              ),
+
+
+
+
+
+              SizedBox(height: 10),
+
+
+
+
+
+              TextField(
+
+                decoration: InputDecoration(
+
+                  hintText: 'Enter your email',
+
+                  filled: true,
+
+                  fillColor: Color(0xFFF1F1F5),
+
+
+
+                  border: OutlineInputBorder(
+
+                    borderRadius: BorderRadius.circular(20),
+
+                    borderSide: BorderSide.none,
+
+                  ),
+
+
+
+                  contentPadding: EdgeInsets.symmetric(
+
+                    horizontal: 20,
+
+                    vertical: 18,
 
                   ),
 
                 ),
 
-
-
-
-
-                SizedBox(height: 20),
+              ),
 
 
 
 
 
-                Text(
-
-                  'Email Address:',
+              SizedBox(height: 15),
 
 
-                  style: TextStyle(
 
-                    fontSize: 18,
 
-                    fontWeight: FontWeight.bold,
+
+              Text(
+
+                'Password:',
+
+
+                style: TextStyle(
+
+                  fontSize: 18,
+
+                  fontWeight: FontWeight.bold,
+
+                ),
+
+              ),
+
+
+
+
+
+              SizedBox(height: 10),
+
+
+
+
+
+              TextField(
+
+                obscureText: true,
+
+
+                decoration: InputDecoration(
+
+                  hintText: 'Enter your Password',
+
+                  filled: true,
+
+                  fillColor: Color(0xFFF1F1F5),
+
+
+
+                  border: OutlineInputBorder(
+
+                    borderRadius: BorderRadius.circular(20),
+
+                    borderSide: BorderSide.none,
+
+                  ),
+
+
+
+                  contentPadding: EdgeInsets.symmetric(
+
+                    horizontal: 20,
+
+                    vertical: 18,
 
                   ),
 
                 ),
 
+              ),
 
 
 
 
-                SizedBox(height: 10),
 
+              Align(
 
+                alignment: Alignment.topLeft,
 
 
-
-                TextField(
-
-                  decoration: InputDecoration(
-
-                    hintText: 'Enter your email',
-
-                    filled: true,
-
-                    fillColor: Color(0xFFF1F1F5),
-
-
-                    border: OutlineInputBorder(
-
-                      borderRadius: BorderRadius.circular(20),
-
-                      borderSide: BorderSide.none,
-
-                    ),
-
-
-                    contentPadding: EdgeInsets.symmetric(
-
-                      horizontal: 20,
-
-                      vertical: 18,
-
-                    ),
-
-                  ),
-
-                ),
-
-
-
-
-
-                SizedBox(height: 15),
-
-
-
-
-
-                Text(
-
-                  'Password:',
-
-
-                  style: TextStyle(
-
-                    fontSize: 18,
-
-                    fontWeight: FontWeight.bold,
-
-                  ),
-
-                ),
-
-
-
-
-
-                SizedBox(height: 10),
-
-
-
-
-
-                TextField(
-
-                  obscureText: true,
-
-
-                  decoration: InputDecoration(
-
-                    hintText: 'Enter your Password',
-
-                    filled: true,
-
-                    fillColor: Color(0xFFF1F1F5),
-
-
-                    border: OutlineInputBorder(
-
-                      borderRadius: BorderRadius.circular(20),
-
-                      borderSide: BorderSide.none,
-
-                    ),
-
-
-                    contentPadding: EdgeInsets.symmetric(
-
-                      horizontal: 20,
-
-                      vertical: 18,
-
-                    ),
-
-                  ),
-
-                ),
-
-
-
-
-
-                SizedBox(height: 5),
-
-
-
-
-
-                TextButton(
+                child: TextButton(
 
                   onPressed: (){
 
@@ -296,60 +297,60 @@ class SignIn extends StatelessWidget {
 
                 ),
 
-
-
-
-
-                SizedBox(height: 20),
+              ),
 
 
 
 
 
-                Center(
-
-                  child: SizedBox(
-
-                    width: 250,
-
-                    height: 60,
-
-
-                    child: ElevatedButton(
-
-                      onPressed: (){},
+              SizedBox(height: 20),
 
 
 
-                      style: ElevatedButton.styleFrom(
-
-                        backgroundColor: Colors.black,
-
-                        foregroundColor: Colors.white,
 
 
-                        shape: RoundedRectangleBorder(
+              Center(
 
-                          borderRadius: BorderRadius.circular(0),
+                child: SizedBox(
 
-                        ),
+                  width: 250,
+
+                  height: 60,
+
+
+                  child: ElevatedButton(
+
+                    onPressed: (){},
+
+
+
+                    style: ElevatedButton.styleFrom(
+
+                      backgroundColor: Colors.black,
+
+                      foregroundColor: Colors.white,
+
+
+
+                      shape: RoundedRectangleBorder(
+
+                        borderRadius: BorderRadius.circular(0),
 
                       ),
 
+                    ),
 
 
-                      child: Text(
 
-                        'Login',
+                    child: Text(
 
+                      'Login',
 
-                        style: TextStyle(
+                      style: TextStyle(
 
-                          fontSize: 25,
+                        fontSize: 25,
 
-                          fontWeight: FontWeight.w500,
-
-                        ),
+                        fontWeight: FontWeight.w500,
 
                       ),
 
@@ -359,60 +360,60 @@ class SignIn extends StatelessWidget {
 
                 ),
 
-
-
-
-
-                SizedBox(height: 20),
+              ),
 
 
 
 
 
-                Center(
-
-                  child: SizedBox(
-
-                    width: 300,
+              SizedBox(height: 20),
 
 
-                    child: TextButton(
-
-                      onPressed: (){
 
 
-                        Navigator.push(
 
-                          context,
+              Center(
 
-                          MaterialPageRoute(
+                child: SizedBox(
 
-                            builder: (context)=> const SignUpPage(),
-
-                          ),
-
-                        );
+                  width: 300,
 
 
-                      },
+                  child: TextButton(
+
+                    onPressed: (){
 
 
-                      child: Text(
+                      Navigator.push(
 
-                        'Create a new account',
+                        context,
 
+                        MaterialPageRoute(
 
-                        style: TextStyle(
-
-                          color: Colors.black,
-
-                          fontSize: 18,
-
-                          fontWeight: FontWeight.w500,
-
-                          decoration: TextDecoration.underline,
+                          builder: (context)=> const SignUpPage(),
 
                         ),
+
+                      );
+
+
+                    },
+
+
+                    child: Text(
+
+                      'Create a new account',
+
+
+                      style: TextStyle(
+
+                        color: Colors.black,
+
+                        fontSize: 18,
+
+                        fontWeight: FontWeight.w500,
+
+                        decoration: TextDecoration.underline,
 
                       ),
 
@@ -422,11 +423,11 @@ class SignIn extends StatelessWidget {
 
                 ),
 
+              ),
 
 
-              ],
 
-            ),
+            ],
 
           ),
 
