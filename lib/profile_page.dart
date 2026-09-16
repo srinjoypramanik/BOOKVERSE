9a799dc59f2d087ad10c0573d67db4f7ed841bd1
Sheikh Tanzid Ahmed Sadi <tanzid01716@gmail.com>
2026-09-16 12:13:14 +0600

    Add button for Settings

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,269 +1,138 @@
 import 'package:flutter/material.dart';
 import 'package:bookverse/CartPage.dart';
 import 'package:bookverse/homescreen.dart';
+import 'settings.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-
       backgroundColor: Colors.white,
 
-
-      appBar: AppBar(
-
-        backgroundColor: Colors.white,
-
-
-        centerTitle: true,
-
-      ),
-
-
+      appBar: AppBar(backgroundColor: Colors.white, centerTitle: true),
 
       body: Center(
-
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.start,
 
           crossAxisAlignment: CrossAxisAlignment.center,
 
-
           children: [
-
-
             SizedBox(height: 50),
 
-
-
-            Icon(
-
-              Icons.person,
-
-              size: 100,
-
-              color: Colors.black,
-
-            ),
-
-
-
+            Icon(Icons.person, size: 100, color: Colors.black),
 
             SizedBox(height: 20),
-
-
 
             Text(
               "USER NAME",
 
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
 
-
-
             SizedBox(height: 5),
-
-
 
             Text(
               "user@gmail.com",
 
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.grey,
-              ),
+              style: TextStyle(fontSize: 16, color: Colors.grey),
             ),
-
-
 
             SizedBox(height: 140),
 
-
-
-
             TextButton(
-
-              onPressed: (){
-
+              onPressed: () {
                 Navigator.push(
-
                   context,
 
-                  MaterialPageRoute(
-
-                    builder: (context)=> const Cart(),
-
-                  ),
-
+                  MaterialPageRoute(builder: (context) => const Cart()),
                 );
-
               },
 
-
               child: Text(
-
                 "My Orders",
 
                 style: TextStyle(
-
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
 
                   decoration: TextDecoration.underline,
-
                 ),
-
               ),
-
             ),
-
-
-
-
 
             SizedBox(height: 20),
 
-
-
-
             TextButton(
-
-              onPressed: (){
-
-
-              },
-
+              onPressed: () {},
 
               child: Text(
-
                 "Wishlist",
 
                 style: TextStyle(
-
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
 
                   decoration: TextDecoration.underline,
-
                 ),
-
               ),
-
             ),
-
-
-
-
-
 
             SizedBox(height: 20),
 
-
-
-
             TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
 
-              onPressed: (){
-
-
+                  MaterialPageRoute(builder: (context) => const Settings()),
+                );
               },
 
-
               child: Text(
-
                 "Settings",
 
                 style: TextStyle(
-
                   fontSize: 20,
                   fontWeight: FontWeight.bold,
 
                   decoration: TextDecoration.underline,
-
                 ),
-
               ),
-
             ),
-
-
-
-
 
             SizedBox(height: 120),
 
-
-
-
             SizedBox(
-
               height: 50,
 
               width: 200,
 
-
               child: ElevatedButton(
-
-
-                onPressed: (){
-
-
+                onPressed: () {
                   Navigator.push(
-
                     context,
 
-                    MaterialPageRoute(
-
-                      builder: (context)=> const HomeScreen(),
-
-                    ),
-
+                    MaterialPageRoute(builder: (context) => const HomeScreen()),
                   );
-
-
                 },
 
-
                 style: ElevatedButton.styleFrom(
-
                   backgroundColor: Colors.black,
 
                   shape: RoundedRectangleBorder(
-
                     borderRadius: BorderRadius.zero,
-
                   ),
-
                 ),
-
-
 
                 child: Text(
-
                   "Logout",
 
-
-                  style: TextStyle(
-
-                    color: Colors.white,
-
-                    fontSize: 25,
-
-                  ),
-
+                  style: TextStyle(color: Colors.white, fontSize: 25),
                 ),
-
-
               ),
-
             ),
           ],
         ),
--- lib/settings.dart
+++ lib/settings.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+
+
+class Settings extends StatelessWidget {
+
+  const Settings({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+
+
+
+      );
+  }
+
+}
