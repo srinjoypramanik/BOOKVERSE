5e480b748cdd4d59e1e2291be953104bd6f41c50
Srinjoy <srinjoypramanik01me@gmail.com>
2026-09-17 09:06:22 +0600

    Update homescreen.dart

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -608,10 +608,14 @@ class HomeScreen extends StatelessWidget {
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text('BRAND NEW',
-                            style: TextStyle(
-                              fontSize: 12,
-                            ),
+                        Container(
+                          color: Colors.black,
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 8,
+                            vertical: 4,
+                          ),
+
+                          
                         ),
                         Text('1000/-',
                           style: TextStyle(
