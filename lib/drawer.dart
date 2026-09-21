5c88c6ffcf172acdfa4ba5bdd183a7126bba9db9
Srinjoy <63208459+srinjoypramanik@users.noreply.github.com>
2026-09-21 23:31:16 +0600

    Add 'My Wishlist' and 'Account' ListTiles to drawer

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -77,6 +77,35 @@ class AppDrawer extends StatelessWidget {
               Navigator.pop(context);
             },
           ),
+          
+          ListTile(
+            leading: Icon(Icons.receipt_long_rounded,
+                color: Colors.black),
+            title: Text('My Wishlist',
+              style: TextStyle(fontWeight: FontWeight.bold,
+                  fontSize: 16),
+            ),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+
+          ListTile(
+            leading: Icon(Icons.person_3_rounded,
+                color: Colors.black),
+            title: Text('Account',
+              style: TextStyle(fontWeight: FontWeight.bold,
+                  fontSize: 16),
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const ProfilePage()),
+              );
+            },
+          ),
+
 
 
 
