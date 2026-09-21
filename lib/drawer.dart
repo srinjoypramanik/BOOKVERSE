85249637ae3a55d8b5d95cf59494679a8e2ab980
Srinjoy <63208459+srinjoypramanik@users.noreply.github.com>
2026-09-21 23:29:46 +0600

    Implement AppDrawer widget for navigation
    Added AppDrawer widget with a custom Drawer layout.

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
+import 'CartPage.dart';
+import 'profile_page.dart';
+import 'settings.dart';
+import 'about_us.dart';
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Colors.white,
+      child: Column(
+        children: [
+
+          Container(
+            height: 150,
+            color: Colors.black,
+            child: Center(
+              child: Image.asset('assets/images/bookverse.png',
+                color: Colors.white,
+                height: 46,
+              ),
+            ),
+          ),
+
 
