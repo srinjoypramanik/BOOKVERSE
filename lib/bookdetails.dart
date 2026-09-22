c4e057cbaa10ef070036c3aa390826208dd1db56
Srinjoy <63208459+srinjoypramanik@users.noreply.github.com>
2026-09-22 23:05:58 +0600

    Add back button and shopping bag icon to book details

--- lib/bookdetails.dart
+++ lib/bookdetails.dart
@@ -30,3 +30,34 @@ class BookDetails extends StatelessWidget{
           onPressed: () {
             Navigator.pop(context);
           },
+           icon: const Icon(
+              Icons.arrow_back,
+              color: Colors.black,
+              size: 35),
+        ),
+
+        centerTitle: true,
+        title: Text('BOOK METRICS',
+          style: TextStyle(
+            color: Colors.grey,
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+           ),
+          ),
+
+
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(
+                      builder: (context)=>const Cart(),
+                  ),
+              );
+            },
+            icon: const Icon(
+              Icons.shopping_bag_outlined,
+              color: Colors.black,
+              size: 35,
+            ),
+          ),
