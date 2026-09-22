953ad9e36018fe2e82b601e137bb0f382de589ba
Sheikh Tanzid Ahmed Sadi <tanzid01716@gmail.com>
2026-09-22 15:21:01 +0600

    fix

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-//import 'package:bookverse/language.dart';
-import 'package:bookverse/theme.dart';
-import 'package:bookverse/privacy.dart';
-import 'package:bookverse/feedback.dart';
+
 
 class Settings extends StatelessWidget {
   const Settings({super.key});
