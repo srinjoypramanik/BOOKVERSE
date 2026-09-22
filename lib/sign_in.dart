550397a8e8e53cdaf2931cc5765c0faa49aaa311
Sheikh Tanzid Ahmed Sadi <tanzid01716@gmail.com>
2026-09-22 20:13:06 +0600

    Fix_Sign_In_page

--- lib/sign_in.dart
+++ lib/sign_in.dart
@@ -1,153 +1,358 @@
 import 'package:flutter/material.dart';
 import 'sign_up.dart';
 
-class SignIn extends StatelessWidget {
+
+class SignIn extends StatefulWidget {
+
   const SignIn({super.key});
+
+
+  @override
+  State<SignIn> createState() => _SignInState();
+
+}
+
+
+
+class _SignInState extends State<SignIn> {
+
+
+  String email = "";
+  String password = "";
+
+
+  bool isLoginActive = false;
+
+
+
+  void checkLoginButton(){
+
+
+    setState(() {
+
+
+      isLoginActive =
+          email.isNotEmpty &&
+              password.isNotEmpty;
+
+
+    });
+
+
+  }
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
+
       backgroundColor: Colors.white,
 
+
       appBar: AppBar(
+
         backgroundColor: Colors.white,
 
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.black),
 
-          onPressed: () {
+        leading: IconButton(
+
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+
+
+          onPressed: (){
+
             Navigator.pop(context);
+
           },
+
         ),
+
       ),
 
+
+
       body: SafeArea(
+
         child: Padding(
+
           padding: EdgeInsets.symmetric(horizontal: 20),
 
+
           child: Column(
+
             crossAxisAlignment: CrossAxisAlignment.start,
 
+
             children: [
+
+
+
               SizedBox(
+
                 height: 100,
 
+
                 child: Center(
+
                   child: Image.asset(
+
                     'assets/images/bookverse.png',
 
                     height: 300,
 
                     width: 300,
+
                   ),
+
                 ),
+
               ),
+
+
+
+
 
               SizedBox(height: 20),
 
-              Center(
-                child: TextButton(
-                  onPressed: () {
 
-                  },
+
+
+
+              Center(
+
+                child: TextButton(
+
+                  onPressed: () {},
+
 
                   child: Text(
+
                     'Sign in with your email',
 
+
                     style: TextStyle(
+
                       fontSize: 20,
 
                       fontWeight: FontWeight.w900,
 
                       decoration: TextDecoration.underline,
+
                     ),
+
                   ),
+
                 ),
+
               ),
+
+
+
+
 
               SizedBox(height: 20),
 
+
+
+
+
               Text(
+
                 'Email Address:',
 
-                style: TextStyle(fontSize: 18,
-                    fontWeight: FontWeight.bold
+
+                style: TextStyle(
+
+                  fontSize: 18,
+
+                  fontWeight: FontWeight.bold,
+
                 ),
+
               ),
+
+
+
+
 
               SizedBox(height: 10),
 
+
+
+
+
               TextField(
+
+
+                onChanged: (value){
+
+
+                  email = value;
+
+
+                  checkLoginButton();
+
+
+                },
+
+
+
                 decoration: InputDecoration(
+
                   hintText: 'Enter your email',
 
+
                   filled: true,
+
 
                   fillColor: Color(0xFFF1F1F5),
 
+
                   border: OutlineInputBorder(
+
                     borderRadius: BorderRadius.circular(20),
 
                     borderSide: BorderSide.none,
+
                   ),
 
+
                   contentPadding: EdgeInsets.symmetric(
+
                     horizontal: 20,
 
                     vertical: 18,
+
                   ),
+
                 ),
+
               ),
+
+
+
+
 
               SizedBox(height: 15),
 
+
+
+
+
               Text(
+
                 'Password:',
 
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+
+                style: TextStyle(
+
+                  fontSize: 18,
+
+                  fontWeight: FontWeight.bold,
+
+                ),
+
               ),
+
+
+
+
 
               SizedBox(height: 10),
 
+
+
+
+
               TextField(
+
+
                 obscureText: true,
 
+
+                onChanged: (value){
+
+
+                  password = value;
+
+
+                  checkLoginButton();
+
+
+                },
+
+
+
                 decoration: InputDecoration(
+
                   hintText: 'Enter your Password',
+
 
                   filled: true,
 
+
                   fillColor: Color(0xFFF1F1F5),
 
+
                   border: OutlineInputBorder(
+
                     borderRadius: BorderRadius.circular(20),
 
                     borderSide: BorderSide.none,
+
                   ),
 
+
                   contentPadding: EdgeInsets.symmetric(
+
                     horizontal: 20,
 
                     vertical: 18,
+
                   ),
+
                 ),
+
               ),
+
+
+
+
 
               SizedBox(height: 15),
 
+
+
+
+
               TextButton(
+
                 onPressed: () {},
 
+
                 style: TextButton.styleFrom(
+
                   padding: EdgeInsets.zero,
 
                   alignment: Alignment.centerLeft,
+
                 ),
 
+
                 child: Text(
+
                   'Forgot My Password',
 
+
                   style: TextStyle(
+
                     color: Colors.grey,
 
                     fontSize: 16,
@@ -155,65 +360,133 @@ class SignIn extends StatelessWidget {
                     fontWeight: FontWeight.bold,
 
                     decoration: TextDecoration.underline,
+
                   ),
+
                 ),
+
               ),
+
+
+
+
 
               SizedBox(height: 20),
 
+
+
+
+
               Center(
+
                 child: SizedBox(
+
                   width: 250,
 
                   height: 60,
 
+
                   child: ElevatedButton(
+
+
                     onPressed: () {},
 
+
+
                     style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.black,
+
+
+                      backgroundColor: isLoginActive
+
+                          ? Colors.black
+
+                          : Color(0xFF2563EB),
+
+
 
                       foregroundColor: Colors.white,
 
+
+
                       shape: RoundedRectangleBorder(
+
                         borderRadius: BorderRadius.circular(30),
+
                       ),
+
                     ),
 
+
+
                     child: Text(
+
                       'Login',
 
+
                       style: TextStyle(
+
                         fontSize: 25,
 
                         fontWeight: FontWeight.w500,
+
                       ),
+
                     ),
+
+
                   ),
+
                 ),
+
               ),
+
+
+
+
 
               SizedBox(height: 20),
 
+
+
+
+
               Center(
+
                 child: SizedBox(
+
                   width: 300,
 
+
                   child: TextButton(
-                    onPressed: () {
+
+
+                    onPressed: (){
+
+
                       Navigator.push(
+
                         context,
 
                         MaterialPageRoute(
-                          builder: (context) => const SignUpPage(),
+
+                          builder: (context)=> const SignUpPage(),
+
                         ),
+
                       );
+
+
                     },
 
+
+
                     child: Text(
+
                       'Create a new account',
 
+
                       style: TextStyle(
+
                         color: Colors.black,
 
                         fontSize: 18,
@@ -221,15 +494,29 @@ class SignIn extends StatelessWidget {
                         fontWeight: FontWeight.w500,
 
                         decoration: TextDecoration.underline,
+
                       ),
+
                     ),
+
                   ),
+
                 ),
+
               ),
+
+
+
             ],
+
           ),
+
         ),
+
       ),
+
     );
+
   }
+
 }
