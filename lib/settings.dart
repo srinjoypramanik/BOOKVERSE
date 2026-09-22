f0789971bd0db355e13144f47f4d699b3e4cc58e
Sheikh Tanzid Ahmed Sadi <tanzid01716@gmail.com>
2026-09-22 15:18:31 +0600

    Fix Settings

--- lib/settings.dart
+++ lib/settings.dart
@@ -373,3 +373,136 @@ class Feedback_page extends StatelessWidget{
   }
 }
 
+
+
+class Privacy extends StatelessWidget{
+  const Privacy ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+      ),
+
+      body: Padding(
+
+
+        padding: EdgeInsets.all(20),
+
+
+        child: Column(
+
+
+          crossAxisAlignment: CrossAxisAlignment.start,
+
+
+          children: [
+
+
+
+            Text(
+
+              "Privacy Policy",
+
+              style: TextStyle(
+
+                fontSize: 28,
+
+                fontWeight: FontWeight.bold,
+
+                color: Colors.black,
+
+              ),
+
+            ),
+
+
+
+
+            SizedBox(height: 20),
+
+
+
+
+
+            Text(
+
+              "BookVerse respects your privacy. We keep your personal information safe and do not share your information with others without permission.",
+
+
+              style: TextStyle(
+
+                fontSize: 18,
+
+                color: Colors.black,
+
+              ),
+
+            ),
+
+
+
+
+            SizedBox(height: 20),
+
+
+
+
+
+            Text(
+
+              "Your data is used only to provide a better reading and shopping experience.",
+
+
+              style: TextStyle(
+
+                fontSize: 18,
+
+                color: Colors.black,
+
+              ),
+
+            ),
+
+
+            SizedBox(height: 30,),
+
+            Text('Thanks For Support us.',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.black,
+              ),
+            ),
+
+
+          ],
+
+
+        ),
+
+
+      ),
+
+
+    );
+
+
+  }
+}
+
+
+
+class Themepage extends StatelessWidget{
+  const Themepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+}
+
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,14 +1,14 @@
-import 'package:flutter/material.dart';
-
-class Themepage extends StatelessWidget{
-  const Themepage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+//
+// class Themepage extends StatelessWidget{
+//   const Themepage({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.white,
+//       ),
+//     );
+//   }
+// }
