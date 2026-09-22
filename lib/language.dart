f6cb7b1c15e3eb24fa37f1da020426d16f1ecbc9
Sheikh Tanzid Ahmed Sadi <tanzid01716@gmail.com>
2026-09-22 15:13:56 +0600

    Fix

--- lib/language.dart
+++ lib/language.dart
@@ -1,65 +1,65 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class Language extends StatelessWidget{
-  const Language({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-      ),
-
-      body: Column(
-        children: [
-          ListTile(
-
-            title: Text(
-
-              "English (UK)",
-
-              style: TextStyle(
-
-                fontSize: 22,
-
-                color: Colors.black,
-
-              ),
-
-            ),
-
-            onTap: (){
-
-            },
-
-          ),
-
-          ListTile(
-
-            title: Text(
-
-              "English (USA)",
-
-              style: TextStyle(
-
-                fontSize: 22,
-
-                color: Colors.black,
-
-              ),
-
-            ),
-
-            onTap: (){
-
-            },
-
-          ),
-
-
-        ],
-      ),
-    );
-
-  }
-}
+// class Language extends StatelessWidget{
+//   const Language({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.white,
+//       ),
+//
+//       body: Column(
+//         children: [
+//           ListTile(
+//
+//             title: Text(
+//
+//               "English (UK)",
+//
+//               style: TextStyle(
+//
+//                 fontSize: 22,
+//
+//                 color: Colors.black,
+//
+//               ),
+//
+//             ),
+//
+//             onTap: (){
+//
+//             },
+//
+//           ),
+//
+//           ListTile(
+//
+//             title: Text(
+//
+//               "English (USA)",
+//
+//               style: TextStyle(
+//
+//                 fontSize: 22,
+//
+//                 color: Colors.black,
+//
+//               ),
+//
+//             ),
+//
+//             onTap: (){
+//
+//             },
+//
+//           ),
+//
+//
+//         ],
+//       ),
+//     );
+//
+//   }
+// }
--- lib/settings.dart
+++ lib/settings.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:bookverse/language.dart';
+//import 'package:bookverse/language.dart';
 import 'package:bookverse/theme.dart';
 import 'package:bookverse/privacy.dart';
 import 'package:bookverse/feedback.dart';
@@ -129,6 +129,70 @@ class Settings extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class Language extends StatelessWidget{
+  const Language({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+      ),
+
+      body: Column(
+        children: [
+          ListTile(
+
+            title: Text(
+
+              "English (UK)",
+
+              style: TextStyle(
+
+                fontSize: 22,
+
+                color: Colors.black,
+
+              ),
+
+            ),
+
+            onTap: (){
+
+            },
+
+          ),
+
+          ListTile(
+
+            title: Text(
+
+              "English (USA)",
+
+              style: TextStyle(
+
+                fontSize: 22,
+
+                color: Colors.black,
+
+              ),
+
+            ),
+
+            onTap: (){
+
+            },
+
+          ),
+
+
+        ],
+      ),
+    );
+
   }
 }
 
