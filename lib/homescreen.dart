9cccc911f1fab9b2136bd0624eb1f8f1380a9c58
Srinjoy <srinjoypramanik01me@gmail.com>
2026-09-25 00:04:58 +0600

    Update homescreen.dart

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -289,8 +289,6 @@ class HomeScreen extends StatelessWidget {
                               ),
                               ElevatedButton(
                                 onPressed: (){
-
-
                                 },
                                 style: ElevatedButton.styleFrom(
                                   backgroundColor: Colors.black,
@@ -325,7 +323,7 @@ class HomeScreen extends StatelessWidget {
                         builder: (context)=> const BookDetails(
                             title: 'ARTIFICIAL INTELLIGENCE: A MODERN \nAPPROACH',
                             author: 'Stuart Russell, Peter Norvig',
-                            price:  '800/-',
+                            price:  '\$8.00',
                             category: 'COMPUTER SCIENCE',
                             description: 'Artificial Intelligence: A Modern Approach is a textbook'
                                 ' about artificial intelligence by Stuart Russell and Peter Norvig.'
@@ -353,84 +351,7 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
 
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                          color: Colors.black,
-                          padding: EdgeInsets.symmetric(
-                            horizontal: 8,
-                            vertical: 4,
-                          ),
-
-                          child: Text('BRAND NEW',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 12,
-                            ),
-                          ),
-                        ),
-
-                        Text('800/-',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-
-                    SizedBox(height: 12),
-                    const Text('ARTIFICIAl INTELLIGENCE: A MODERN APPROACH',
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 6),
-                    const Text('Stuart Russell, Peter Norvig',
-                      style: TextStyle(
-                        color: Colors.grey,
-                      ),
-                    ),
-
-                    SizedBox(height: 1),
-                    const Divider(thickness: 0.5,),
-                    SizedBox(height: 1),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text('COMPUTER SCIENCE',
-                          style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 14,
-                          ),
-                        ),
-                        ElevatedButton(
-                          onPressed: (){},
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.black,
-                            foregroundColor: Colors.white,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.zero,
-                            ),
-                          ),
-                          child: const Text('+ADD TO CART',
-                            style: TextStyle(
-                              fontSize: 13,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+                
               ),
             ),
 
