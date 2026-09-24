87d10f5565042eb879d7cdb8501c3ddbbb90fb62
Srinjoy <srinjoypramanik01me@gmail.com>
2026-09-25 00:18:43 +0600

    Update homescreen.dart

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -477,8 +477,95 @@ class HomeScreen extends StatelessWidget {
                     color: Colors.black,
                   ),
                 ),
+                child: Row(
+                  children: [
+                    Image.asset('name',
+                    width: 110,
+                    height: 198,
+                    fit: BoxFit.fitWidth,
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                color: Colors.black,
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: 8,
+                                  vertical: 4,
+                                ),
 
-               
+                                child: Text('BRAND NEW',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ),
+                              Text('1000/-',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+
+                          SizedBox(height: 12),
+                          const Text('CLEAN CODE: A HANDBOOK OF SOFTWARE CRAFTSMANSHIPS',
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 6),
+                          const Text('Robert C. Martin',
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+
+                          SizedBox(height: 1),
+                          const Divider(thickness: 0.5,),
+                          SizedBox(height: 1),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text('COMPUTER SCIENCE',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 14,
+                                ),
+                              ),
+                              ElevatedButton(
+                                onPressed: (){},
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.black,
+                                  foregroundColor: Colors.white,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.zero,
+                                  ),
+                                ),
+                                child: const Text('+ADD TO CART',
+                                  style: TextStyle(
+                                    fontSize: 13,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
 
