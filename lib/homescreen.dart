eea8967ae33a2a89996099c8966a81629b72d71d
Srinjoy <srinjoypramanik01me@gmail.com>
2026-09-26 05:00:33 +0600

    Update homescreen.dart

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -619,6 +619,138 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
             ),
+            // EE Book 2
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context,
+                  MaterialPageRoute(
+                    builder: (context)=> const BookDetails(
+                      title: 'ELECTRONIC DEVICES AND CIRCUIT \nTHEORY',
+                      author: 'by Robert L. Boylestad, Louis Nashelsky',
+                      price:  '\$12.00',
+                      category: 'ELECTRICAL & ELECTRONICS',
+                      description: 'Electronic Devices and Circuit Theory, originally '
+                          'written by Robert L. Boylestad and Louis Nashelsky, is widely'
+                          ' regarded as the definitive standard textbook for engineering '
+                          'students and electronics professionals. For decades, it has '
+                          'provided a comprehensive and highly accessible introduction to '
+                          'semiconductor devices and their practical applications. The '
+                          'book systematically covers everything from the foundational '
+                          'physics of diodes and transistors (BJTs and FETs) to advanced '
+                          'systems like operational amplifiers, feedback circuits, and '
+                          'power supplies. It balances mathematical rigor with intuitive '
+                          'explanations, making complex circuit analysis techniques highly '
+                          'digestible.',
+                      image: 'assets/images/elec_device.jpg',
+                    ),
+                  ),
+                );
+              },
+              child: Container(
+                margin: EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 8,
+                ),
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.black,
+                  ),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Row(
+                  children: [
+                    Image.asset('assets/images/elec_device.jpg',
+                      width: 110,
+                      height: 198,
+                      fit: BoxFit.fitWidth,
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Container(
+                                color: Colors.black,
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: 8,
+                                  vertical: 4,
+                                ),
+
+                                child: Text('BRAND NEW',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ),
+
+                              Text('\$10.00',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+
+                          SizedBox(height: 12),
+                          const Text('ELECTRONIC DEVICES AND CIRCUIT THEORY ',
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 6),
+                          const Text('Robert L. Boylestad, Louis Nashelsky',
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              color: Colors.grey,
+                            ),
+                          ),
+
+                          SizedBox(height: 1),
+                          const Divider(thickness: 0.5,),
+                          SizedBox(height: 1),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              const Text('ELECTRICAL & \nELECTRONICS',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 14,
+                                ),
+                              ),
+                              ElevatedButton(
+                                onPressed: (){},
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.black,
+                                  foregroundColor: Colors.white,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.zero,
+                                  ),
+                                ),
+                                child: const Text('+ADD TO CART',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
 
             //Book3
             GestureDetector(
@@ -878,138 +1010,7 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
             ),
-            // EE Book 1
-            GestureDetector(
-              onTap: () {
-                Navigator.push(context,
-                  MaterialPageRoute(
-                    builder: (context)=> const BookDetails(
-                      title: 'FUNDAMENTALS OF ELECTRIC CIRCUITS,\n6TH EDITION',
-                      author: 'by Charles K. Alexander, Matthew Sadiku',
-                      price:  '\$10.00',
-                      category: 'ELECTRICAL & ELECTRONICS',
-                      description: 'Fundamentals of Electric Circuits by Charles K. Alexander '
-                          'and Matthew N. O. Sadiku is a benchmark introductory engineering '
-                          'textbook published by McGraw-Hill that offers a clear, comprehensive,'
-                          ' and student-focused introduction to linear circuit analysis. The '
-                          'text is strategically organized into three distinct parts, '
-                          'sequentially guiding students through the foundational physics of '
-                          'DC circuits, the steady-state analysis of AC circuits, and advanced '
-                          'network analysis methods like Laplace and Fourier transforms. '
-                          'Renowned for its pedagogical clarity, the book introduces a '
-                          'signature six-step problem-solving methodology that trains '
-                          'students to systematically approach complex electrical engineering'
-                          ' challenges.',
-                      image: 'assets/images/fundamen_circuit.jpg',
-                    ),
-                  ),
-                );
-              },
-              child: Container(
-                margin: EdgeInsets.symmetric(
-                  horizontal: 16,
-                  vertical: 8,
-                ),
-                padding: EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black,
-                  ),
-                  borderRadius: BorderRadius.circular(5),
-                ),
-                child: Row(
-                  children: [
-                    Image.asset('assets/images/fundamen_circuit.jpg',
-                      width: 110,
-                      height: 198,
-                      fit: BoxFit.fitWidth,
-                    ),
-                    SizedBox(width: 10),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Container(
-                                color: Colors.black,
-                                padding: EdgeInsets.symmetric(
-                                  horizontal: 8,
-                                  vertical: 4,
-                                ),
 
-                                child: Text('BRAND NEW',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 12,
-                                  ),
-                                ),
-                              ),
-
-                              Text('\$10.00',
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ],
-                          ),
-
-                          SizedBox(height: 12),
-                          const Text('FUNDAMENTALS OF ELECTRIC CIRCUITS, 6TH EDITION ',
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(height: 6),
-                          const Text('Charles K. Alexander, Matthew Sadiku',
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ),
-                          ),
-
-                          SizedBox(height: 1),
-                          const Divider(thickness: 0.5,),
-                          SizedBox(height: 1),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              const Text('ELECTRICAL & \nELECTRONICS',
-                                style: TextStyle(
-                                  color: Colors.grey,
-                                  fontSize: 14,
-                                ),
-                              ),
-                              ElevatedButton(
-                                onPressed: (){},
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.black,
-                                  foregroundColor: Colors.white,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.zero,
-                                  ),
-                                ),
-                                child: const Text('+ADD TO CART',
-                                  style: TextStyle(
-                                    fontSize: 12,
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
           ],
         ),
       ),
