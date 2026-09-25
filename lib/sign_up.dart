a9494d52069521b5efe86aaa9a7cee9d928e30ae
Sheikh Tanzid Ahmed Sadi <tanzid01716@gmail.com>
2026-09-25 21:09:50 +0600

    Update sign_up.dart

--- lib/sign_up.dart
+++ lib/sign_up.dart
@@ -74,7 +74,8 @@ class _SignUpPageState extends State<SignUpPage> {
     } on FirebaseAuthException catch (e) {
       ScaffoldMessenger.of(
         context,
-      ).showSnackBar(SnackBar(content: Text(e.message ?? "Signup Failed",),),;
+      ).showSnackBar(SnackBar(content: Text(e.message ?? "Signup Failed",
+      ),),);
     }
   }
 
