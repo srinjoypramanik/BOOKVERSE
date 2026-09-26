fbedcc7df628457cb5806d76231d1efc67b34d12
Sheikh Tanzid Ahmed Sadi <tanzid01716@gmail.com>
2026-09-26 16:29:07 +0600

    Fix

--- lib/feedback.dart
+++ lib/feedback.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+
 
 class Feedback_page extends StatelessWidget{
   const Feedback_page ({super.key});
